df8180418b037f2fda186a728cce614bffe5840c
Mohammad Attique <mattique02@outlook.com>
2025-03-12 23:40:00 +0500

    cleanup

--- frontend/app/Work/page.tsx
+++ frontend/app/Work/page.tsx
@@ -2,7 +2,7 @@ import { About } from "@/components/sections/About";
 import Footer from "@/components/layout/Footer"
 import  Navbar  from "@/components/layout/navbar";
 import { Hero } from "@/components/sections/Hero";
-import { TwoColumn } from "@/components/sections/TwoColumn";
+
 
 export default function Home() {
   return (
